cd8b287ccbce9864489266caf80a5707fcbc25de
Caleb <caleb542@gmail.com>
2023-01-18 13:20:43 -0500

    adding reopitem and github actions to the codebase

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,24 +1,29 @@
 import {useState, useContext} from 'react'
 import GithubContext from '../../context/github/GithubContext'
 import AlertContext from '../../context/alert/AlertContext'
+import { searchUsers } from '../../context/github/GithubActions'
 
 
-function UserSearch() {
+function UserSearch () {
     const [text, setText] = useState('')
 
-    const {users, searchUsers, clearUsers} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
 
     const {setAlert} = useContext(AlertContext) 
 
     const handleChange = (e) => setText(e.target.value)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        
         if(text === ''){
-            setAlert("Don't leave the form field blank, WRITE SOMETHING!", "error")
+            setAlert("Please enter something", "error")
         } else {
             //  - search users
-            searchUsers(text)
+            dispatch({type: 'SET_LOADING'})
+            const users = await searchUsers(text)
+
+            dispatch({type: 'GET_USERS', payload: users})
             setText('')
         }
     }
@@ -42,7 +47,7 @@ function UserSearch() {
         </div>
         {users.length > 0 && (
             <div>
-                <button onClick={clearUsers} className="btn btn-ghost btn-large">Clear</button>
+                <button onClick={() => dispatch({type: 'CLEAR_USERS'})} className="btn btn-ghost btn-large">Clear</button>
             </div>
         )}
         
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -3,101 +3,25 @@ import { createContext,  useReducer  } from "react";
 import githubReducer from './GithubReducer';
 const GithubContext = createContext()
 
-const GITHUB_URL = import.meta.env.VITE_REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({children}) => {
 
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
 
-    // Get search results
-    const searchUsers = async (text) => {
-        setLoading()
-        const params = new URLSearchParams({
-            q: text
-        }) 
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,{
-            headers: {
-                Authorization: `${GITHUB_TOKEN}`
-            },
-        })
-
-        const {items} = await response.json()
-
-       dispatch({
-        type: 'GET_USERS',
-        payload: items,
-       })
-    }
-
-    // Get Single User
-
-    const getUser = async (login) => {
-        setLoading()
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}`,{
-            headers: {
-                Authorization: `${GITHUB_TOKEN}`
-            },
-        })
-
-        if(response.status === 404) {
-            window.location = '/notfound'
-        }   else {
-            const data = await response.json()
-
-            dispatch({
-                type: 'GET_USER',
-                payload: data,
-            })
-        }
-    }
-
-     // Get user repos
-     const getUserRepos = async (login) => {
-        setLoading()
-        
-        const params = new URLSearchParams({
-            sort : 'date',
-            per_page: '10'
-        }) 
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos`,{
-            headers: {
-                Authorization: `${GITHUB_TOKEN}`
-            },
-        })
-
-        const data = await response.json()
-
-       dispatch({
-        type: 'GET_REPOS',
-        payload: data,
-       })
-    }
-    // set loading 
-    const setLoading = () => dispatch({type:'SET_LOADING'})
-    
-    // clear users from state
-    const  clearUsers = () => dispatch({type:'CLEAR_USERS'})
 
     return (
         <GithubContext.Provider 
-        value={{
-            ...state,
-            searchUsers, 
-            clearUsers,
-            getUser,
-            getUserRepos,
-        }}
+            value={{
+                ...state,
+                dispatch,
+            }}
         >
             {children}
         </GithubContext.Provider>)
